dff9d7d4d644cf9de96fa7655f97ce7666882a2f
Ray Sakai <ray.sakai@logical-fabrics.com>
2016-01-26 19:18:44 +0900

    closes #1, closes #2

--- public/javascripts/stores/AudioStore.js
+++ public/javascripts/stores/AudioStore.js
@@ -1,6 +1,15 @@
 'use strict';
+import _ from 'lodash';
 import Reflux from 'reflux';
+import request from 'xhr-request';
+
 import AudioAction from '../actions/AudioAction';
+
+const DATA_SIZES = {
+  drum: 3,
+  bass: 1,
+  se: 1
+};
 
 export default Reflux.createStore({
   listenables: [AudioAction],
@@ -9,7 +18,14 @@ export default Reflux.createStore({
     context: null,
     oscNode: null,
     gainNode: null,
-    playing: false
+    playing: false,
+
+    isLoading: false,
+    audio: {
+      drum: [],
+      bass: [],
+      se: []
+    }
   },
 
   start() {
@@ -41,9 +57,49 @@ export default Reflux.createStore({
 
     this.res.gainNode.connect(this.res.context.destination);
     this.res.oscNode.start();
+
+    this.loadAudioData();
   },
 
   getInitialState() {
     return this.res;
+  },
+
+  loadAudioData() {
+    // Loading Drum
+    const drumP = new Promise((resolve, reject) => {
+      _.forEach(_.range(DATA_SIZES.drum), (num) => {
+        request(`/audio/drum${num}.mp3`, {responseType: 'arraybuffer'}, (err, data) => {
+          this.res.audio.drum.push(data);
+          resolve();
+        })
+      })
+    });
+
+    // Loading Bass
+    const bassP = new Promise((resolve, reject) => {
+      _.forEach(_.range(DATA_SIZES.bass), (num) => {
+        request(`/audio/bass${num}.mp3`, {responseType: 'arraybuffer'}, (err, data) => {
+          this.res.audio.bass.push(data);
+          resolve();
+        })
+      })
+    });
+
+    // Loading SE
+    const seP = new Promise((resolve, reject) => {
+      _.forEach(_.range(DATA_SIZES.se), (num) => {
+        request(`/audio/se${num}.mp3`, {responseType: 'arraybuffer'}, (err, data) => {
+          this.res.audio.se.push(data);
+          resolve();
+        })
+      })
+    });
+
+    // Mark as loading done
+    Promise.all([drumP, bassP, seP]).then(() => {
+      this.res.isLoading = true;
+      this.trigger(this.res);
+    });
   }
 })
